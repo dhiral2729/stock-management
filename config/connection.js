e53688a532b345a413b9ceaf60f05902e490b5b1
dhiral2729 <dhiral.prajapati2003@gmail.com>
2025-04-22 20:10:00 +0530

    new commen fetures are add

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectTomongodb = async (url) => {
   mongoose
-    .connect('mongodb+srv://rahilsaiyed1711:rG60ylVvlbJjGWGu@cluster0.mi8dtk0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .connect(process.env.MONGO_URI)
     .then(() => console.log('mongo connected:'))
     .catch((err) => {
       console.log(err);
