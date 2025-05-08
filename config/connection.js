8619515395f631232a3ec1e0ce691af6865092c6
dhiral2729 <dhiral.prajapati2003@gmail.com>
2025-05-08 11:57:59 +0530

    atlas complated

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const MongoUrl = process.env.MONGO_URI;
 
 const connectTomongodb = async (req,res) => {
   mongoose
-    .connect('mongodb://localhost:27017/sms')
+    .connect('mongodb+srv://dhiralprajapati2003:aiRDfMG5l3W12q3b@cluster0.pbe9ezq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => console.log('mongo connected:'))
     .catch((err) => {
       console.log(err);
