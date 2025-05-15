107a344ea421bd1551a2f0dbeb4ceb488c7eb0e6
dhiral2729 <dhiral.prajapati2003@gmail.com>
2025-05-15 14:22:16 +0530

    added some port config

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 require('dotenv').config();
 const app=express()
-const port=process.env.PORT 
+const port=process.env.PORT || 3000
 const{ connectTomongodb}=require("./config/connection")
 connectTomongodb()
 app.use(express.json())
