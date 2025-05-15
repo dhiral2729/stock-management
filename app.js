3704081838ed46e527e694be9f214b0a039e5fd1
dhiral2729 <dhiral.prajapati2003@gmail.com>
2025-05-15 14:03:41 +0530

    demo stock

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 require('dotenv').config();
 const app=express()
-const port=5000
+const port=process.env.PORT || "5000"
 const{ connectTomongodb}=require("./config/connection")
 connectTomongodb()
 app.use(express.json())
